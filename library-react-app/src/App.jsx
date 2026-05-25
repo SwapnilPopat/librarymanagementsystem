ec604e3fa00d76bd79cef3aba275ac9439f3c860
Popat, Swapnil  AE/PUN-RM524 <popatswapnil@gmail.com>
2026-05-25 14:42:50 +0530

    feat: implement BookList component and enhance book management functionality

--- library-react-app/src/App.jsx
+++ library-react-app/src/App.jsx
@@ -1,32 +1,54 @@
 import { useState } from "react";
+import BookList from "./BookList";
+import "./App.css";
 
 function App() {
-  const [books, setBooks] = useState(["Clean Code", "Harry Potter", "The Alchemist","Mastery"]);
-  const [newBook,setNewBook] = useState("");
+  const [books, setBooks] = useState([
+    "Clean Code",
+    "Harry Potter",
+    "The Alchemist",
+  ]);
+  const [newBook, setNewBook] = useState("");
+  const [search, setSearch] = useState("");
 
-  function handleAddBook(){
-    if(newBook.trim() === "") return;
-    setBooks([...books,newBook]);
+  function handleAddBook() {
+    if (newBook.trim() === "") return;
+    setBooks([...books, newBook]);
     setNewBook("");
   }
 
+  function handleDeleteBook(indexToDelete) {
+    const updatedBooks = books.filter((book, index) => index !== indexToDelete);
+    setBooks(updatedBooks);
+  }
+
+  const filteredBooks = books.filter((book) =>
+    book.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
-    <div>
+    <div className="container">
       <h1>Library Management App</h1>
       <h2>Book List</h2>
-      <input
-          type="text"
-          placeholder="Enter new book name"
-          value={newBook}
-          onChange={(e) => setNewBook(e.target.value)} />
 
-      <ul>
-        {
-        books.map((book, index) => (<li key={index}>{book}</li>))
-        }
-      </ul>
+      <input
+        type="text"
+        placeholder="Enter book name"
+        value={newBook}
+        onChange={(e) => setNewBook(e.target.value)}
+      />
       <button onClick={handleAddBook}>Add Book</button>
+
+      <br /><br />
+
+      <input
+        type="text"
+        placeholder="Search book"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <BookList books={filteredBooks} onDelete={handleDeleteBook} />
     </div>
   );
 }
